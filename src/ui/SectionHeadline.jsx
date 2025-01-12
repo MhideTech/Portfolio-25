248c5fb07c281ca56eb3e7da8bbb99ba5d8197b1
MhideTech <okefolahanolamide2006@gmail.com>
2025-01-12 23:52:03 +0100

    modified section headline

--- src/ui/SectionHeadline.jsx
+++ src/ui/SectionHeadline.jsx
@@ -3,11 +3,9 @@ function SectionHeadline() {
     <div className="flex items-center justify-center my-8">
       <div className="border-t border-gray-300 flex-grow"></div>
 
-      <div className="px-4">
-        <div className="transform rotate-[-15deg] bg-white px-6 py-2 text-center shadow-lg">
-          <span className="font-medium text-gray-800">Services</span>
+        <div className="transform rotate-[-12deg] bg-white px-8 py-3 text-center shadow-lg rounded-full">
+          <span className="font-medium text-gray-800 text-xl">Services</span>
         </div>
-      </div>
 
       <div className="border-t border-gray-300 flex-grow"></div>
     </div>
