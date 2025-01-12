0ea610e4b595c7f8e1ef1145b848a066bb67619e
MhideTech <okefolahanolamide2006@gmail.com>
2025-01-12 23:49:19 +0100

    created section header

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,33 +1,39 @@
 import Header from "../ui/Header";
 import Mouse from "../ui/Mouse";
 import NameBadge from "../ui/NameBadge";
+import SectionHeadline from "../ui/SectionHeadline";
 
 function Homepage() {
   return (
-    <div className="p-2 h-screen bg-[#8d8d8b]">
-      <div className="h-full background relative flex flex-col justify-center items-center w-full rounded">
-        <Header />
-        <div className="w-40 h-40 rounded-full border-4 border-gray-600 relative flex mb-10 hover:grayscale-[60%]">
-          <img
-            src="src/assets/images/me2.jpeg"
-            alt="Okefolahan Olamide Israel"
-            className="w-full h-full rounded-full object-cover pointer-events-none"
-          />
-          <NameBadge />
+    <>
+      <div className="p-2 h-screen bg-[#8d8d8b]">
+        <div className="h-full background relative flex flex-col justify-center items-center w-full rounded">
+          <Header />
+          <div className="w-40 h-40 rounded-full border-4 border-gray-600 relative flex mb-10 hover:grayscale-[60%]">
+            <img
+              src="src/assets/images/me2.jpeg"
+              alt="Okefolahan Olamide Israel"
+              className="w-full h-full rounded-full object-cover pointer-events-none"
+            />
+            <NameBadge />
+          </div>
+
+          <h1 className="text-white text-6xl mb-5 font-bold tracking-wide w-2/4 text-center">
+            Fullstack Web and Mobile App Developer
+          </h1>
+          <ul className="list-disc text-white flex gap-20 text-4xl font-bold mt-5 test">
+            <li className="">iBuild</li>
+            <li>iCreate</li>
+            <li>iDesign</li>
+          </ul>
+
+          <Mouse />
         </div>
-
-        <h1 className="text-white text-6xl mb-5 font-bold tracking-wide w-2/4 text-center">
-          Fullstack Web and Mobile App Developer
-        </h1>
-        <ul className="list-disc text-white flex gap-20 text-4xl font-bold mt-5 test">
-          <li className="">iBuild</li>
-          <li>iCreate</li>
-          <li>iDesign</li>
-        </ul>
-
-        <Mouse />
       </div>
-    </div>
+      <br /><br /><br /><br /><br />
+      <SectionHeadline />
+      <br /><br /><br /><br /><br />
+    </>
   );
 }
 
---
+++ src/ui/SectionHeadline.jsx
@@ -0,0 +1,17 @@
+function SectionHeadline() {
+  return (
+    <div className="flex items-center justify-center my-8">
+      <div className="border-t border-gray-300 flex-grow"></div>
+
+      <div className="px-4">
+        <div className="transform rotate-[-15deg] bg-white px-6 py-2 text-center shadow-lg">
+          <span className="font-medium text-gray-800">Services</span>
+        </div>
+      </div>
+
+      <div className="border-t border-gray-300 flex-grow"></div>
+    </div>
+  );
+}
+
+export default SectionHeadline;
