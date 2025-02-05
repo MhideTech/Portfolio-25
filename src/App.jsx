145e328f4c98a6161df96daf090b7f6457b8c13e
MhideTech <okefolahanolamide2006@gmail.com>
2025-02-05 17:10:39 +0100

    created loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import "animate.css";
-
+import { useEffect, useState } from "react";
+import Loader from "./ui/Loader";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setIsLoading(false);
+    };
+
+    window.addEventListener("load", handleLoad);
+
+    return () => {
+      window.removeEventListener("load", handleLoad);
+    };
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <BrowserRouter>
       <Routes>
--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,7 +1,11 @@
 import "./../styles/loader.css"
 
 function Loader() {
-  return <div className="loader"></div>;
+  return (
+    <div className="h-screen flex justify-center items-center bg-gray-800">
+      <div className="loader text-3xl"></div>
+    </div>
+  );
 }
 
 export default Loader;
