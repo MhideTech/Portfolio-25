4b922740067903378229573bf033b7ddf8bbc437
MhideTech <okefolahanolamide2006@gmail.com>
2025-02-06 10:09:55 +0100

    fixing loader

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,9 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const timeout = setTimeout(() => setIsLoading(false), 8000); // Hide loader after 8s
-
-    const checkReadyState = () => {
-      if (document.readyState === "complete") {
-        setIsLoading(false);
-        clearTimeout(timeout);
-      }
-    };
-
-    document.addEventListener("readystatechange", checkReadyState);
-    window.addEventListener("load", checkReadyState);
-
-    return () => {
-      document.removeEventListener("readystatechange", checkReadyState);
-      window.removeEventListener("load", checkReadyState);
-      clearTimeout(timeout);
-    };
+    window.addEventListener("load", function () {
+      setIsLoading(false);
+    });
   }, []);
 
   if (isLoading) {
