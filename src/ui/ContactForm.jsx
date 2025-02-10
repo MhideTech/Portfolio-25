9619ee8f128d38c2d571b6f89f37ecf4bb0ac194
MhideTech <okefolahanolamide2006@gmail.com>
2025-02-10 22:33:08 +0100

    created contact form

--- src/ui/ContactForm.jsx
+++ src/ui/ContactForm.jsx
@@ -1,5 +1,15 @@
 function ContactForm() {
-  return <form>Contact Me Form</form>;
+  return (
+    <form>
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" placeholder="Enter your name" />
+      <label htmlFor="email">Email</label>
+      <input type="email" name="email" id="email" />
+      <label htmlFor="message">Message</label>
+      <textarea name="message" id="message"></textarea>
+      <button type="submit">Reach out to me</button>
+    </form>
+  );
 }
 
 export default ContactForm;
