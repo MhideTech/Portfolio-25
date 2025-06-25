354097d17b0695f656d1d05db4c63750e549a1a6
MhideTech <okefolahanolamide2006@gmail.com>
2025-06-25 18:42:36 +0100

    updated project lists

--- src/ui/Project.jsx
+++ src/ui/Project.jsx
@@ -6,13 +6,12 @@ import Button from "./Button";
 function Project() {
   const projects = [
     {
-      image: "/images/projects-image/e-learn.png",
-      name: "QuizWhiz",
-      url: "",
-      description:
-        "An online learning platform that allow students to learn in a fun and interactive way through quizzes",
-      technologies: "Tailwind CSS, ReactJS, TypeScript, Node.js, PostgreSQL",
-      githubUrl: "https://github.com/MhideTech/quizwhiz-frontend",
+      image: "/images/projects-image/video-call.png",
+      name: "Duo Video Share",
+      url: "https://duo-share-v2.vercel.app/",
+      description: "A featured simple P2P video sharing app",
+      technologies: "JavaScript, ReactJS, TailwindCSS, WebRTC",
+      githubUrl: "https://github.com/MhideTech/duo_share_v2",
     },
     {
       image: "/images/projects-image/plant-palace.png",
@@ -22,6 +21,23 @@ function Project() {
         "An e-commerce web application for the purchase of various plant for indoor and outdoor decorations",
       technologies: "HTML, CSS, JavaScript",
       githubUrl: "https://github.com/MhideTech/Techwiz-Project-PlantPalace",
+    },
+    {
+      image: "/images/projects-image/e-learn.png",
+      name: "QuizWhiz",
+      url: "",
+      description:
+        "An online learning platform that allow students to learn in a fun and interactive way through quizzes",
+      technologies: "Tailwind CSS, ReactJS, TypeScript, Node.js, PostgreSQL",
+      githubUrl: "https://github.com/MhideTech/quizwhiz-frontend",
+    },
+    {
+      image: "/images/projects-image/e-library.png",
+      name: "E-Library",
+      url: "",
+      description: "An online e-library management for book lovers",
+      technologies: "JavaScript, TypeScript, ReactJS, TailwindCSS",
+      githubUrl: "https://github.com/MhideTech/online-bookstore",
     },
     {
       image: "/images/projects-image/text-redaction.png",
